fc2f0c68d90855023e4241d605aea6d9a1bb377a
Ta Van Dinh <dinhtv7@fpt.edu.vn>
2024-01-19 09:07:28 +0700

    Buổi học 8

---
+++ Route/controller/TrangChuController.js
@@ -0,0 +1,7 @@
+window.TrangChuController = function($scope) {
+    $scope.title = "Thầy Định đẹp trai";
+
+    // Tạo 1 form nhập vào 2 số, và 1 button
+    // Sau khi nhấn submit thì sẽ tính tổng 2 số 
+    // và hiển thị ra màn hình
+}
---
+++ Route/router.js
@@ -0,0 +1,28 @@
+let myApp = angular.module('myApp', ['ngRoute']);
+// ngRoute: cung cấp các tính năng định tuyến (routing)
+// dùng để quản lý việc điều hướng
+myApp.config(function ($routeProvider) {
+    // config() cấu hình tham số trước khi chạy ứng dụng
+    // $routeProvider là tham số mặc định khô thể thay đổi tên
+    // Dùng để định nghĩa ra các route
+    $routeProvider
+        .when(
+            '/trang-chu', // Tên route
+            {
+                templateUrl: 'view/trangchu.html', // Đường dẫn đến view
+                controller: TrangChuController // tên controller cần map đến view
+            }
+        )
+        .when(
+            '/gioi-thieu', // Tên route
+            {
+                templateUrl: 'view/gioithieu.html', // Đường dẫn đến view
+            }
+        )
+        .otherwise({
+            // Trong trường hợp người dùng truy cập
+            // 1 route ko đc định nghĩa
+            // nó sẽ tự động chuyển vào route này
+            redirectTo: '/trang-chu'
+        })
+})
